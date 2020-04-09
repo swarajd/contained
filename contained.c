cd8db3ab17a0ce30ea64c8013481967140f33beb
swarajd <swaraj.dhumne@gmail.com>
2020-04-09 18:08:55 -0400

    added headers to main file + gitignore

---
+++ contained.c
@@ -0,0 +1,24 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <pwd.h>
+#include <sched.h>
+#include <seccomp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/capability.h>
+#include <sys/mount.h>
+#include <sys/prctl.h>
+#include <sys/resource.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <linux/capability.h>
+#include <linux/limits.h>
+
